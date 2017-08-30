ce4e36cb079efbbd24c96376f5ffd1e67638d242
Ricardo Alvize <Ricardo Alvize@DESKTOP-0T85HB8>
2017-08-30 19:55:47 -0300

    Parte2

--- ProjetoWeb/App_Start/BundleConfig.cs
+++ ProjetoWeb/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ProjetoWeb
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.simplex.css",
+                      "~/Content/bootstrap.cyborg.css",
                       "~/Content/site.css"));
         }
     }
